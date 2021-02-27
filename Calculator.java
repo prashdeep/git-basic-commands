4aa867cca31d04a17fcfd11f5a50a2eb8f759ec2
pradeep <pradeep.kumar44@gmail.com>
2021-02-28 00:24:40 +0530

    committing the sub method in branch b

--- Calculator.java
+++ Calculator.java
@@ -26,4 +26,8 @@ public class Calculator {
     public int sum(int a, int b, int c){
         return a + b + c;
     }
+    
+    public int sub(int a, int b, int c){
+        return a - b - c;
+    }
 }
