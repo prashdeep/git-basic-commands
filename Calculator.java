16a8ab48af25276ba028e2cc400b61e9605eaa2c
pradeep <pradeep.kumar44@gmail.com>
2021-02-28 00:59:55 +0530

    committing the product overloaded method

--- Calculator.java
+++ Calculator.java
@@ -30,5 +30,8 @@ public class Calculator {
     public int sub(int a, int b, int c){
         return a - b - c;
     }
-    
+
+    public int product(int a, int b, int c){
+        return a * b * c;
+    }
 }
