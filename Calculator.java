f5ad1c233d17c5f5c70c094e6a53b89b97945214
pradeep <pradeep.kumar44@gmail.com>
2021-03-05 23:20:27 +0530

    committing the changes

--- Calculator.java
+++ Calculator.java
@@ -11,27 +11,4 @@ public class Calculator {
         return a * b;
     }
 
-    public int subtract(int a, int b){
-        return a - b;
-    }
-
-    public int modulo(int a, int b){
-        return a % b;
-    }
-    
-    public double divide ( int a, int b){
-        return a / b;
-    }
-
-    public int sum(int a, int b, int c){
-        return a + b + c;
-    }
-    
-    public int sub(int a, int b, int c){
-        return a - b - c;
-    }
-
-    public int product(int a, int b, int c){
-        return a * b * c;
-    }
 }
